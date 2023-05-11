4f5fc8c4a0a84aae24b490624d24213a475963fc
Abu-Taieb <taieb.contact.bd@gmail.com>
2023-05-11 21:14:34 +0600

    Log in Page Designed Complete

---
+++ src/Pages/Login/Login.jsx
@@ -0,0 +1,63 @@
+import loginImage from "../../assets/images/login/login.svg";
+
+const Login = () => {
+
+    const handleLogin = event => {
+        event.preventDefault();
+    }
+
+  return (
+    <div className="hero min-h-screen bg-base-200 py-10 lg:py-0">
+      <div className="hero-content flex flex-col-reverse lg:flex-row gap-20">
+        <div className="lg:w-1/2 ">
+          <img src={loginImage} alt="Log In" />
+        </div>
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <div className="card-body">
+            <h2 className="text-4xl font-bold text-center">Login</h2>
+            <form onSubmit={handleLogin}>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="email"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="password"
+                  className="input input-bordered"
+                  required
+                />
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">
+                    Forgot password?
+                  </a>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+                <input
+                  className="btn bg-[#FF3811] border-[#FF3811]"
+                  type="submit"
+                  value="Login"
+                />
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
